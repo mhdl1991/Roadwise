7eb6db3702b708d81ba0b5c29f1583d044d9a2ad
mhdl1991 <mohammad.ali1991@live.com>
2025-05-27 01:46:54 +0100

    Adding some icons

--- AboutPage.xaml.cs
+++ AboutPage.xaml.cs
@@ -2,7 +2,13 @@ namespace Roadwise;
 
 public partial class NewPage1 : ContentPage
 {
-	public NewPage1()
+    private async void LearnMore_Clicked(object sender, EventArgs e)
+    {
+        // Navigate to the specified URL in the system browser.
+        await Launcher.Default.OpenAsync("https://aka.ms/maui");
+    }
+
+    public NewPage1()
 	{
 		InitializeComponent();
 	}
