179f5d7a86a1207ef13697459b93a8411f9ab16d
mhdl1991 <mohammad.ali1991@live.com>
2025-05-29 18:52:21 +0100

    ????

--- NewDrawable.cs
+++ NewDrawable.cs
@@ -11,13 +11,21 @@ using Microsoft.Maui.Graphics.Platform;
 namespace Roadwise
 {
     public class NewDrawable : IDrawable
-    {
-        public NewDrawable() { 
+    {   
+        private List<(string id, PointF area)> Hotspots;
+        public NewDrawable() {
             // What if I were to create a list of hotspots here
-        
+            // Define clickable zones
+            Hotspots = [
+                ("Point1", new(100, 150)),
+                ("Point2", new(300, 200)),
+                ("Point3", new(200, 400))
+            ];
         }
         public void Draw(ICanvas canvas, RectF dirtyRect)
         {
+            canvas.SaveState();
+
             IImage image;
             Assembly assembly = GetType().GetTypeInfo().Assembly;
             using (Stream stream = assembly.GetManifestResourceStream("Roadwise.Resources.Images.map.jpg"))
@@ -29,6 +37,17 @@ namespace Roadwise
             {
                 canvas.DrawImage(image, 10, 10, image.Width, image.Height);
             }
+
+            foreach ((string id, PointF p) in Hotspots)
+            {
+                canvas.StrokeColor = Colors.Black;
+                canvas.StrokeSize = 4;
+                canvas.FillColor = Colors.Red;
+                canvas.FillEllipse(p.X, p.Y, 25, 25);
+                canvas.DrawEllipse(p.X, p.Y, 25, 25);
+            }
+
+            canvas.RestoreState();
         }
     }
 }
